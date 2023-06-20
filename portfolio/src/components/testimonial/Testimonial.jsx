3ec9e3018e3995174b20da464498cd135eb5ad86
kelagb45 <agbornkongho356@gmail.com>
2023-06-20 17:49:10 +0100

    downloaded

--- portfolio/src/components/testimonial/Testimonial.jsx
+++ portfolio/src/components/testimonial/Testimonial.jsx
@@ -13,25 +13,23 @@ const data = [
     avatar: AVTR1,
     name: "Ekema Ashley",
     review:
-      "I was so happy with the photos that kelly took of my wedding. she captured the day perfectly and I will cherish him forever.",
+      "I was so happy with the photos that kelly took of my wedding. she captured the day perfectly and I will cherish her forever.",
   },
   {
     avatar: AVTR2,
     name: "Brenda",
     review:
-      "kelly is a true artist. He has a great eye for composition and lighting, and she knows how to capture the beauty in every moment.",
+      "kelly is a true artist. she has a great eye for composition and lighting, and she knows how to capture the beauty in every moment.",
   },
   {
     avatar: AVTR3,
     name: "Malando Tina",
-    review:
-      "I was very happy with the work that kelly did on my website. she was able to take my vision and turn it into a reality. The website is exactly what I wanted and it looks great. I would definitely recommend HarryShots to anyone who is looking for a web developer.",
+    review:"I was very happy with the work that kelly did on my website. she was able to take my vision and turn it into a reality. The website is exactly what I wanted and it looks great. I would definitely recommend kellyShots to anyone who is looking for a web developer.",
   },
   {
     avatar: AVTR4,
     name: "Makoun Glory",
-    review:
-      "kelly was very easy to work with. He was always responsive to my feedback and she was always willing to make changes to the content until I was happy with the results.",
+    review:"kelly was very easy to work with. shee was always responsive to my feedback and she was always willing to make changes to the content until I was happy with the results.",
   },
 ];
  
